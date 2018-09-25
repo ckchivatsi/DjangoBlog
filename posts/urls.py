7e931af8f4501b456829acbc39a474e39a348810
ckchivatsi <kelvinchivatsi@hotmail.com>
2018-09-25 15:43:41 +0300

    Created posts details page

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url('', views.index, name='index'),
+    url('details/', views.details, name='details')
 ]
--- posts/views.py
+++ posts/views.py
@@ -15,3 +15,13 @@ def index(request):
     }
 
     return render(request, 'posts/index.html', context)
+
+def details(request, id):
+    post = Posts.objects.get(id=id)
+
+    context = {
+        'title': 'Technology Posts',
+        'post': post  
+    }
+    
+    return render(request, 'posts/details.html', context)
