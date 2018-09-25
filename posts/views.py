32777687b6ace61fd3932084ee2ae4e10fd585d2
ckchivatsi <kelvinchivatsi@hotmail.com>
2018-09-25 15:21:38 +0300

    View posts from database

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Posts
+
 # Create your views here.
 def index(request):
     # return HttpResponse('HELLO FROM POSTS')
 
-    return render(request, 'posts/index.html', {
-        'title': 'Technology Posts'
-    })
+    posts = Posts.objects.all()[:10]
+
+    context = {
+        'title': 'Technology Posts',
+        'posts': posts  
+    }
+
+    return render(request, 'posts/index.html', context)
