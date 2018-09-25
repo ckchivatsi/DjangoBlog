aa2d93c16db75776514b3e121f611d13819feb0e
ckchivatsi <kelvinchivatsi@hotmail.com>
2018-09-25 12:40:36 +0300

    MySQL Connection

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -73,13 +73,22 @@ WSGI_APPLICATION = 'DjangoBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DjangoBlog',
+        'USER': 'DjangoBlog',
+        'PASSWORD': 'djangoblog09',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
